d372fd217d6b6437c44949273895aae905bc3ffa
Daniel Cabrera <danielfcabrerac@gmail.com>
2022-04-20 21:13:52 -0500

    adding changes to react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./DC.jpg";
+import "./App.css";
+import TitleComponent from "./components/TitleComponent";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <TitleComponent></TitleComponent>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Modificado <code>src/App.js</code> y cambiado por DC para ADA
         </p>
         <a
           className="App-link"
---
+++ src/components/TitleComponent.jsx
@@ -0,0 +1,8 @@
+function TitleComponent() {
+  return (
+    <div>
+      <h1>Mi 1er app</h1>
+    </div>
+  );
+}
+export default TitleComponent;
